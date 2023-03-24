4b435363799a982f85dc8ca07289e8f9b9349865
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-03-24 14:44:55 +0530

    Compression Algo

--- CompressorDecompresser/src/comp/decomp/compressor.java
+++ CompressorDecompresser/src/comp/decomp/compressor.java
@@ -10,16 +10,22 @@ import java.util.zip.GZIPOutputStream;
 public class compressor {
 
     public static void method(File file) throws IOException {
+        //get parent path
         String fileDirectory=file.getParent();
-
+        
+        //read textfile
         FileInputStream fis=new FileInputStream(file);
+        //create new file to write with .gz extension
         FileOutputStream fos=new FileOutputStream(fileDirectory+"/CompressedFile.gz");
+        //to compressed the input file by read and write into new one
         GZIPOutputStream gzip=new GZIPOutputStream(fos);
-
+        
+        //chunks of byte to read the whole file
         byte[] buffer = new byte[1024];
         int len;
-
+        
         while((len=fis.read(buffer))!=-1){
+            //write with compression
             gzip.write(buffer, 0, len);
         }
 
